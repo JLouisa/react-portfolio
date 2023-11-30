2d559ef2c94ceb3365a5537325a43bd31a50ed53
Jlouisa <jonathan.louisa@gmail.com>
2023-11-30 09:55:36 +0100

    Adding nextJS boilerplate

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20"></div>
+    </main>
+  );
+}
