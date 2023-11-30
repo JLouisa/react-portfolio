cb10f592e32ad6b8e0b67bf6e7fefd5d56a3ba41
Jlouisa <jonathan.louisa@gmail.com>
2023-11-30 12:24:42 +0100

    Finish hero section

--- components/main/sub/HeroContent.tsx
+++ components/main/sub/HeroContent.tsx
@@ -22,11 +22,16 @@ const HeroContent = () => {
         </motion.div>
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
-          Providing
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">the best</span>
-          Project Experience
+          <span>
+            Providing
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              the best{" "}
+            </span>
+            Project Experience
+          </span>
         </motion.div>
         <motion.p variants={slideInFromLeft(0.8)} className="text-lg text-gray-400 my-5 max-w-[600px]">
           I&apos;m a Fullstack Software Engineer with experience in Website, Mobile and Software Development. Check out
