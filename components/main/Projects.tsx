4d80c1546b2eceb224128831a47473bb7f6780d7
Jlouisa <jonathan.louisa@gmail.com>
2023-12-01 02:03:13 +0100

    Add project section

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import ProjectCard from "./sub/ProjectCard";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="flex flex-col items-center justify-center py-20">
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        My Projects
+      </h1>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        <ProjectCard
+          src="/NextWebsite.png"
+          title="Modern Next.JS Porfolio"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        />
+        <ProjectCard
+          src="/CardImage.png"
+          title="Interactive responsive cards"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        />
+        <ProjectCard
+          src="/SpaceWebsite.png"
+          title="Space Themed Website"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
